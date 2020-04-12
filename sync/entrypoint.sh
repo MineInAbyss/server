b4a3321d0c6f4337a65f21aace1eab13d98a71f7
LizeLive <github@lize.live>
2020-04-11 22:07:27 -0700

    making progress

--- sync/entrypoint.sh
+++ sync/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 crontab scheduler.txt
 cron -f
+
+mkdir -p /diff/upper /diff/work
+
+mkdir /readonly
+mount -t overlay overlay -o lowerdir=/worlds,upperdir=/diff/upper,workdir=/diff/work /readonly
+
+
 echo "I hate my bones"
