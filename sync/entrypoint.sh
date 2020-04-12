c84979101dfbcc22c17e9ffbc1510cd8550a9e44
LizeLive <github@lize.live>
2020-04-12 13:07:48 -0700

    testing sync

--- sync/entrypoint.sh
+++ sync/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+
+
+# ummmm im going to hell
+
+mkdir /sinkhole
+
 crontab scheduler.txt
 cron -f
 
-mkdir -p /diff/upper /diff/work
+mkdir /sinkhole/upper /sinkhole/work
 
 mkdir /readonly
-mount -t overlay overlay -o lowerdir=/worlds,upperdir=/diff/upper,workdir=/diff/work /readonly
+mount -t overlay overlay -o lowerdir=/worlds,upperdir=/sinkhole/upper,workdir=/sinkhole/work /readonly
 
+ln -s /cloud/worlds/test/world /worlds/test/world
+ln -s /readonly/worlds/master/world /worlds/test/master_world
 
 echo "I hate my bones"
+
+bash
