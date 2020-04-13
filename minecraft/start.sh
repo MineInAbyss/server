e62ff8bf623e1c75a2a6021428c03e603260a78f
LizeLive <github@lize.live>
2020-04-12 23:12:28 -0700

    maybe

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -4,17 +4,15 @@
 
 mkdir /sinkhole
 #mount -t nullfs none /sinkhole/
+
 mkdir /sinkhole/upper /sinkhole/workdir
 
 mkdir /readonly
 mount -t overlay overlay -o lowerdir=/cloud,upperdir=/sinkhole/upper,workdir=/sinkhole/work /readonly
 
 
-
-ln -s /cloud/worlds/$UWU/world /worlds/world
-# use 
-ln -s /readonly/worlds/master/world /worlds/master_world
-
-
+# make ls links uwu
+ln /cloud/$UWU/world /worlds/world
+ln /readonly/master/world /worlds/master_world
 
 java ${JAVA_ARGS} -Dcom.mojang.eula.agree=true -jar paper.jar --nojline --nogui -W /worlds -p 25565 
