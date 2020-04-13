ef2960f9180745c9fbc6fb0648247e5afddf9325
LizeLive <github@lize.live>
2020-04-12 20:50:38 -0700

    welp im going to hellp uwu

--- sync/entrypoint.sh
+++ sync/entrypoint.sh
@@ -4,18 +4,19 @@
 # ummmm im going to hell
 
 mkdir /sinkhole
+#mount -t nullfs none /sinkhole/
 
-crontab scheduler.txt
-cron -f
+# crontab scheduler.txt
+# cron -f
 
 mkdir /sinkhole/upper /sinkhole/work
 
 mkdir /readonly
-mount -t overlay overlay -o lowerdir=/worlds,upperdir=/sinkhole/upper,workdir=/sinkhole/work /readonly
+mount -t overlay overlay -o lowerdir=/cloud,upperdir=/sinkhole/upper,workdir=/sinkhole/work /readonly
 
 ln -s /cloud/worlds/test/world /worlds/test/world
 ln -s /readonly/worlds/master/world /worlds/test/master_world
 
 echo "I hate my bones"
 
-bash
+sleep 360
