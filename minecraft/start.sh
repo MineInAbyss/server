d02892f9a2e54b2d1dd7d1b2462e0b5d052dc7c6
LizeLive <github@lize.live>
2020-04-12 23:24:45 -0700

    it's working. time to go full feature uwu.

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+mkdir /worlds
 
-
-mkdir /sinkhole
 #mount -t nullfs none /sinkhole/
+ln -s /tmp /sinkhole
 
-mkdir /sinkhole/upper /sinkhole/workdir
+mkdir /sinkhole/upper /sinkhole/work
 
 mkdir /readonly
 mount -t overlay overlay -o lowerdir=/cloud,upperdir=/sinkhole/upper,workdir=/sinkhole/work /readonly
 
 
 # make ls links uwu
-ln /cloud/$UWU/world /worlds/world
-ln /readonly/master/world /worlds/master_world
+ln -s /cloud/$UWU/world /worlds/world
+ln -s /readonly/master/world /worlds/master_world
 
 java ${JAVA_ARGS} -Dcom.mojang.eula.agree=true -jar paper.jar --nojline --nogui -W /worlds -p 25565 
